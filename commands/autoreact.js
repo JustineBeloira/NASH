d6f01d41f5c159754bee169ed64460dab896e41a
JustineBeloira <148711038+JustineBeloira@users.noreply.github.com>
2024-07-04 01:42:23 +0800

    Update autoreact.js

--- commands/autoreact.js
+++ commands/autoreact.js
@@ -3,7 +3,7 @@ module.exports = {
     description: "basta auto react sya ganun HAHAHA",
     nashPrefix: false,
     execute: async (api, event) => {
-        const introduction = `Hey everyone my name is NASHBOT! Are you tired of manually reacting to posts on Facebook? Try out this Auto FB Reaction app! It automatically reacts to posts for you, making your Facebook experience more convenient and enjoyable.\n\nClick here to check it out: [https://www.apkfiles.com/apk-615022/autoreact]`;
+        const introduction = `Hey everyone my name is 𝙅𝙪𝙨𝙩𝙞𝙣𝙚 𝘿𝙚𝙫! Are you tired of manually reacting to posts on Facebook? Try out this Auto FB Reaction app! It automatically reacts to posts for you, making your Facebook experience more convenient and enjoyable.\n\nClick here to check it out: [https://www.apkfiles.com/apk-615022/autoreact]`;
         api.sendMessage(introduction, event.threadID);
     }
 };
